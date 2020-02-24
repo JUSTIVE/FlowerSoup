c9edf4e4dcbe5c3d14f532e7693455c185c1e58a
JUSTIVE <ben399399@naver.com>
2020-02-24 19:52:51 +0900

    WIP on 1260

--- boj/w1260.fs
+++ boj/w1260.fs
@@ -3,9 +3,9 @@ open System
 let mp s (x,y) =
     match s|>Map.tryFind x with
     | Some a ->
-        s|>Map.add x (a|>Seq.append [y,false])
+        s|>Map.add x (a|>Seq.append [y])
     | None ->
-        s|>Map.add x (Seq.append Seq.empty<int*bool> [(y,false)])
+        s|>Map.add x (Seq.append Seq.empty<int> [y])
 
 let arr_to_pair (x:int[]) =
     match x with
@@ -17,23 +17,15 @@ let rec ins s i c =
     if i = c-1 then s
     else ins (mp s (Console.ReadLine().Split()|>Array.map int|>arr_to_pair)) (i+1) c
 
-type t = Map <int, Map<int, bool>>
-
-
-let rec dfs (g:Map<int,seq<int*bool>>) (vl:seq<int>) b = 
-    g
-    |>Map.toSeq
-    |>Seq.sortBy(fun (a,b)->a)
-    |>Seq.map(fun (a,b)->
-        b
-        |>Seq.map(fun y->printfn " "))
+let rec dfs ()=
+    ignore    
     
-let doProb (d:Map<int,seq<int*bool>>) (b:int) =
-    dfs d Seq.empty<int> b
+let doProb (d:Map<int,seq<int>>) (b:int) =
+    dfs (d,(List.empty<int>|>List.append [b]))
     |>ignore
 
 let [|n;m;v|] = Console.ReadLine().Split()|>Array.map int
-ins Map.empty<int,seq<int*bool>> 0 (Console.ReadLine()|>int)
+ins Map.empty<int,int> 0 (Console.ReadLine()|>int)
 |>Map.map(fun k v -> v|>Seq.sortBy(fun (a,b)->a))
 |>doProb<|v
 
