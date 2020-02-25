da4da4da8e749fcea58341a4b3a233cbc6ef4da8
JUSTIVE <ben399399@naver.com>
2020-02-25 14:05:47 +0900

    WIP on 1260

--- boj/w1260.fs
+++ boj/w1260.fs
@@ -1,11 +1,19 @@
 open System
 
 let mp s (x,y) =
-    match s|>Map.tryFind x with
+    let mutable  aa,bb = 0,0
+    if x>y then
+        aa<-y
+        bb<-x
+    else
+        aa<-x
+        bb<-y
+        
+    match s|>Map.tryFind aa with
     | Some a ->
-        s|>Map.add x (a|>Seq.append [y])
+        s|>Map.add aa (a|>Seq.append [bb])
     | None ->
-        s|>Map.add x (Seq.append Seq.empty<int> [y])
+        s|>Map.add aa (Seq.append Seq.empty<int> [bb])
 
 let arr_to_pair (x:int[]) =
     match x with
@@ -14,19 +22,37 @@ let arr_to_pair (x:int[]) =
     
         
 let rec ins s i c =
-    if i = c-1 then s
+    if i = c then s
     else ins (mp s (Console.ReadLine().Split()|>Array.map int|>arr_to_pair)) (i+1) c
 
-let rec dfs ()=
-    ignore    
+let validCandidate (graph:Map<int,seq<int>>) (index:int) (visitiedList:Set<int>) =
+    match graph|>Map.tryFind index with
+    | Some v ->
+        v
+        |>Seq.toList
+        |>List.filter(fun x-> Set.exists (fun y -> x<>y) visitiedList)
+        |>List.sort
+    | None -> List.empty<int>
+
+let rec dfs graph toVisitList visitiedList =
+    if toVisitList|>List.length = 0 then graph
+    else
+        match toVisitList with
+        | h::t ->
+            printf "%d " h
+            dfs graph (t|>List.append (validCandidate graph h visitiedList)) (visitiedList|>Set.add h)
+        | []->
+            dfs graph List.empty<int> Set.empty<int>
+    
+
     
 let doProb (d:Map<int,seq<int>>) (b:int) =
-    dfs (d,(List.empty<int>|>List.append [b]))
+    dfs d (List.empty<int>|>List.append [b]) (Set.empty<int>|>Set.add b)
     |>ignore
 
 let [|n;m;v|] = Console.ReadLine().Split()|>Array.map int
-ins Map.empty<int,int> 0 (Console.ReadLine()|>int)
-|>Map.map(fun k v -> v|>Seq.sortBy(fun (a,b)->a))
+ins Map.empty<int,seq<int>> 0 m
+|>Map.map(fun k v -> v|>Seq.sort)
 |>doProb<|v
 
 
